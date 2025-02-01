0db409c98e979d39c1bed66bb3c37ed6135daf7d
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2025-02-02 01:28:51 +0900

    setup flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # MP4 動画の圧縮
+            ffmpeg
+            # 動画の情報を取得
+            mediainfo
+            # EXIF meta 情報の読み書き
+            exiftool
+          ];
+        };
+      }
+    );
+}
